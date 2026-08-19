31d01e5def821232f526d33e7a94269fd823a942
devCroaker <37278000+devCroaker@users.noreply.github.com>
2026-08-19 13:55:04 -0700

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        if (nums.length === 1) return [0]
+        if (nums.length === 2) return [nums[1], nums[0]]
+
+        const datums = []
+        let prev = 1
+        for (let i = 0; i < nums.length; i++) {
+            datums.push([prev, 1])
+            prev = prev * nums[i]
+        }
+
+        prev = 1
+        for (let i = nums.length-1; i >= 0; i--) {
+            datums[i][1] = prev
+            prev = prev * nums[i]
+        }
+
+        return datums.map(datum => datum[0] * datum[1])
+    }
+}
